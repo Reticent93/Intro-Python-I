a934bd5064990543c7a09480ee0d1341c6b5c17b
Greg <remph819@gmail.com>
2020-08-11 16:20:09 -0700

    finished project

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,14 +10,17 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [i for i in range(6)]
+print(y)
 
-print (y)
+
+
+
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [i*3 for i in range(10)]
 
 print(y)
 
@@ -26,7 +29,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [s.upper() for s in a]
 
 print(y)
 
@@ -37,6 +40,9 @@ x = input("Enter comma-separated numbers: ").split(',')
 print(x)
 
 # What do you need between the square brackets to make it work?
-y = [int(i) for i in int(i) % 2 == 0]
+y = []
+for i in x:
+    if int(i) % 2 == 0:
+        y.append(int(i))
 
 print(y)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# with open('foo.txt') as f  print(f.read())
+f = open('foo.txt')
+print(f.read())
+f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+y = open("bar.txt", "w")
+y.write("this is first line.\n")
+y.write("this is second line.\n")
+y.write("this is third line.")
+y.close()
+
+with open("bar.txt") as y:
+    print("\n" + y.read())
